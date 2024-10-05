0cf96911dce34d3b8ee1004ebdd62d921118809f
Emma Larsson <emma940112@gmail.com>
2024-10-05 05:38:46 +0200

    Skapat en huvudmeny för applikationen

--- Program.cs
+++ Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //While-loop som gör att programmet fortsätter köras till användaren väljer att avsluta programmet
+            while (true)
+            {
+                Console.Clear(); //Rensa konsollen innan menyn skrivs ut
+                Console.WriteLine("E M M A ' S  Q U I Z\n");
+                Console.WriteLine("[1] Start Quiz");
+                Console.WriteLine("[X] Close application");
+
+                //Läser in användarens val
+                //Trim() tar bort eventuella mellanslag runt användarens inmatning
+                //ToUpper() Säkerställer att både "X" och "x" är giltiga val
+                string choice = Console.ReadLine().Trim().ToUpper();
+
+                //Konsollen rensas innan resultatet visas
+                Console.Clear();
+                //Switch-sats som hanterar användarens val
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Du har valt alternativ 1");
+                        break;
+
+                    case "X":
+                        return;
+
+                    default:
+                        Console.WriteLine("Error: Invalid choice");
+                        break;
+
+                }
+                //Vänta med att rensa konsollen
+                Console.WriteLine("Press a key to continue");
+                Console.ReadKey();
+            }
         }
     }
 }
--- obj/Debug/net8.0/projekt_dt071g.AssemblyInfo.cs
+++ obj/Debug/net8.0/projekt_dt071g.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projekt_dt071g")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39f300537919a7010e7262971f2326b66ad75897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f423b66b7c4e48ea7515fdbc84a586752616767")]
 [assembly: System.Reflection.AssemblyProductAttribute("projekt_dt071g")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projekt_dt071g")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
