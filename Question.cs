3f423b66b7c4e48ea7515fdbc84a586752616767
Emma Larsson <emma940112@gmail.com>
2024-10-05 05:21:22 +0200

    Skapat en klass för frågor

---
+++ Question.cs
@@ -0,0 +1,9 @@
+namespace quiz
+{
+    public class Question
+    {
+        public string? Text { get; set; }
+        public string[]? Answers { get; set; }
+        public int? CorrectAnswerIndex { get; set; }
+    }
+}
